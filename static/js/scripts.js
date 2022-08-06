1e8418a1c2b8ea3aa67f85dc87b529dedc2434a6
Manish Shivabhakti <the.real.manish.1999@gmail.com>
2022-08-06 13:03:23 +0545

    Add phone SMS modal

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,36 @@
-sendToButton = document.getElementById('sendToButton');
+const sendToButton = document.getElementById('sendToButton');
+const backdrop = document.getElementById('backdrop');
+const modal = document.getElementById('modal');
+const closeButton = document.getElementById('closeButton');
+const cancelButton = document.getElementById('cancelButton');
+const sendButton = document.getElementById('sendButton');
+const recordButton = document.getElementById('recordButton');
+const stopButton = document.getElementById('stopButton');
 
-sendToButton.addEventListener('click', function () {
-  console.log('sendToButton clicked');
-});
+const toggleModal = () => {
+  console.log('triggered');
+  backdrop.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+  modal.classList.toggle('flex');
+};
+
+const toggler = () => {
+  stopButton.classList.toggle('hidden');
+  recordButton.classList.toggle('hidden');
+};
+
+const toggleRecordState = () => {
+  if (recordButton.innerText == 'Record a video') {
+    toggler();
+  } else if (stopButton.innerText == 'Stop recording') {
+    toggler();
+  }
+};
+
+recordButton.addEventListener('click', toggleRecordState);
+stopButton.addEventListener('click', toggleRecordState);
+
+sendToButton.addEventListener('click', toggleModal);
+backdrop.addEventListener('click', toggleModal);
+cancelButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
